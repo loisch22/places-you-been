03746776d09b3e949180a65a775ed81437bf2c13
Kim Schulze and Lois Choi <kim@>
2017-07-17 13:30:34 -0700

    try to hide and show travel info

--- js/scripts.js
+++ js/scripts.js
@@ -10,47 +10,36 @@ Place.prototype.displayDestination = function() {
   return this.destination;
 };
 
-// Place.prototype.placesInfo = function() {
-//   return this.locale + " " + this.landmark + " " + this.year + " " + this.notes;
-// }
-
-Place.prototype.makeNewPlace = function(destination, locale, landmark, year, notes) {
-  destination[i] = this.destination;
-  location[i] = this.locale;
-  landmark[i] = this.landmark;
-  year[i] = this.year;
-  notes[i] = this.notes;
-};
+Place.prototype.placesInfo = function() {
+  return this.locale + " " + this.landmark + " " + this.year + " " + this.notes;
+}
 
 $(document).ready(function() {
-  var i = 0;
   $("#new-destination").submit(function(event) {
     event.preventDefault();
-
+debugger;
     var inputtedDestination = $("input#new-destination").val();
     var inputtedLocation = $("input#new-location").val();
     var inputtedLandmark = $("input#new-landmark").val();
     var inputtedYear = $("input#new-year").val();
-    var inputtedNotes = $("input#new-notes").val()
+    var inputtedNotes = $("input#new-notes").val();
 
     var newPlace = new Place(inputtedDestination, inputtedLocation, inputtedLandmark, inputtedYear, inputtedNotes);
 
-    $("ul#placeName").append("<li>" + newPlace.displayDestination() + "</li>");
 
     $("ul#places").append("<li><span class='places'>" + inputtedDestination + "</span></li>" + "<li><span class='places'>" + inputtedLocation + "</span></li>" + "<li><span class='places'>" + inputtedLandmark + "</span></li>" + "<li><span class='places'>" + inputtedYear + "</span></li>" + "<li><span class='places'>" + inputtedNotes + "</span></li>");
 
-    // $("#placeName").click(function(event) {
-    //   $("#places").toggle(newPlace.placesInfo());
-    //
-    //   event.preventDefault();
-    // });
+    $("#placeName").click(function(event) {
+      $("#places").show();
+
+      event.preventDefault();
+    });
 
     $("input#new-destination").val("");
     $("input#new-location").val("");
     $("input#new-landmark").val("");
     $("input#new-year").val("");
     $("input#new-notes").val("");
-
 
   });
 });
